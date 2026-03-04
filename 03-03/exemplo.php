2c024aaba154e7cda119d065f3948afd2dddf15f
Lorente97 <matheuslorente1@gmail.com>
2026-03-03 21:53:20 -0300

    acabamos o exercicio

--- 03-03/exemplo.php
+++ 03-03/exemplo.php
@@ -30,7 +30,34 @@
       else {
         echo "<p>Sinal Inválido</p>";
       }*/
-    }
+
+
+        switch($valor){
+            case"+";
+                echo "<p>Sinal de Soma</p>";
+                //
+             break;
+            case "-";
+                echo "<p>Sinal de Subtração</p>";
+             break;
+            case "*";
+                echo "<p>Sinal de Multiplicação";
+             break;
+            case "/";
+                echo "<p>Sinal de Divisão";
+             break;
+            default:
+                echo "<p>Sinal Inválido!</p>";
+             break;
+        }
+      }
+      for($i=1; $i<=10; $i++){
+        $resultado = $valor * $i;
+        echo "<p> $valor * $i = $resultado </p>";
+      }
+      
+      
+
 ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </div>
