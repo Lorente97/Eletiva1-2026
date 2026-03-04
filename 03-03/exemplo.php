6cc8b6ebc9b24e35d3c436b70c8c039dda977aa0
Lorente97 <matheuslorente1@gmail.com>
2026-03-03 21:33:17 -0300

    fazendo exercicio com a prof

--- 03-03/exemplo.php
+++ 03-03/exemplo.php
@@ -19,7 +19,7 @@
 <?php
     if ($_SERVER['REQUEST_METHOD'] == 'POST'){
       $valor = $_POST['valor'];
-      if($valor == "+")
+      /*if($valor == "+")
         echo "<p>Sinal de soma!</p>";
       elseif($valor == "-")
         echo "<p>Sinal de subtração!</p>";
@@ -29,7 +29,7 @@
         echo "<p>Sinal de divisão!</p>";
       else {
         echo "<p>Sinal Inválido</p>";
-      }
+      }*/
     }
 ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
