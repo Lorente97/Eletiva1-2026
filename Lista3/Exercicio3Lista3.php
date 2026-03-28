fd3cc185617cae3682c9d2e56dba1723e1cd1167
Lorente97 <matheuslorente1@gmail.com>
2026-03-28 15:25:18 -0300

    Fiz o exercicio 3

---
+++ Lista3/Exercicio3Lista3.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title></title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1></h1>
+<form method="post">
+<div class="mb-3">
+              <label for="a" class="form-label">Insira o primeiro valor</label>
+              <input type="text" id="a" name="a" class="form-control" required="">
+            </div><div class="mb-3">
+              <label for="b" class="form-label">Insira o segundo valor:</label>
+              <input type="text" id="b" name="b" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $a = $_POST["a"];
+    $b = $_POST["b"];
+
+    if ($a != "" && $b != "") {
+
+        if ($a == $b) {
+            echo "Números iguais: $a";
+        } else {
+            if ($a < $b) {
+                echo "$a $b";
+            } else {
+                echo "$b $a";
+            }
+        }
+
+    } else {
+        echo "Por favor, insira valores válidos.";
+    }
+}
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
