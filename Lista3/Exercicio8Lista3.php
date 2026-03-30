2d7b7f234ca553f400fb94203234fbf3bcd3872b
Lorente97 <matheuslorente1@gmail.com>
2026-03-29 22:22:29 -0300

    Fiz o exercicio 8 e 9

---
+++ Lista3/Exercicio8Lista3.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title></title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1></h1>
+<form method="post">
+<div class="mb-3">
+              <label for="numero" class="form-label">Informe um Número:</label>
+              <input type="text" id="numero" name="numero" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $numero = $_POST["numero"];
+
+    if ($numero != "" && $numero >= 1) {
+
+        $i = $numero;
+
+        do {
+            echo $i . "<br>";
+            $i--;
+        } while ($i >= 1);
+
+    } else {
+        echo "Por favor, insira um número válido maior ou igual a 1.";
+    }
+}
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
